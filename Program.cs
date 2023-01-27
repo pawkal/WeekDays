80a034baad4d3816a69594113f5c79c2eaac497a
pawkal <kalel7410@gmail.com>
2023-01-27 19:37:46 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wpisz nr dnia tygodnia");
+            string numer = Console.ReadLine();
+            if (numer == "1")
+                Console.WriteLine("Poniedziałek");
+            else if (numer == "2")
+                Console.WriteLine("Wtorek");
+            else if (numer == "3")
+                Console.WriteLine("Środa");
+            else if (numer == "4")
+                Console.WriteLine("Czwartek");
+            else if (numer == "5")
+                Console.WriteLine("Piątek");
+            else if (numer == "6")
+                Console.WriteLine("Sobota");
+            else if (numer == "7")
+                Console.WriteLine("Niedziela");
+            else
+                Console.WriteLine("Nie ma takiego dnia tygodnia");
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
